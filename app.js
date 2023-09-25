56fbf56fa661d3c74a2bc7454cf6ac7bbbdb19f4
m.nasroalah <mehdi.barati@gmail.com>
2023-09-25 16:54:50 +0330

    -

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const PORT = process.env.PORT || 5000;
+
 const express = require("express");
 
 const app = express();
 app.use(express.json());
 
-app.listen(PORT, (port) => {
-  console.log("Server Listening on PORT:", port);
+console.log(PORT);
+app.listen(PORT, () => {
+  console.log("Server Listening on PORT:", PORT);
 });
 
 app.get("/status", (request, response) => {
